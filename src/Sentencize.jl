c04f28f9fe0523336f7e08ee679839c29f192a59
Astariul <remondnicola@gmail.com>
2020-07-27 21:52:26 +0900

    :tada: First working version of the project

--- src/Sentencize.jl
+++ src/Sentencize.jl
@@ -1,20 +1,30 @@
 module Sentencize
 
-export SentenceSplitter
 export SUPPORTED_LANG
+export PrefixType
+export Prefixes
+export split_sentence
 
 const SUPPORTED_LANG = ["ca", "cs", "da", "de", "el", "en", "es", "fi", "fr",
                         "hu", "is", "it", "lt", "lv", "nl", "no", "pl", "pt",
                         "ro", "ru", "sk", "sl", "sv", "tr"]
 
-struct SentenceSplitter
-    non_breaking_prefixes::Array{String}
+@enum PrefixType default numeric_only
 
-    function SentenceSplitter(prefixes::Array{String}=String[]; prefix_file=missing, lang="en")
+struct Prefixes
+    non_breaking_prefixes::Dict{String, PrefixType}
+
+    function Prefixes(prefixes::Dict{String, PrefixType}=Dict{String, PrefixType}(); prefix_file=missing, lang="en")
         function _load_prefix_file(pfile)
-            nb_prefixes = String[]
+            nb_prefixes = Dict{String, PrefixType}()
             open(pfile) do file
                 for line in eachline(file)
+                    if occursin("#NUMERIC_ONLY#", line)
+                        prefix_type = numeric_only
+                    else
+                        prefix_type = default
+                    end
+
                     line = replace(line, r"#.*" => "")     # Remove comments
                     line = strip(line)
     
@@ -22,7 +32,7 @@ struct SentenceSplitter
                         continue
                     end
     
-                    push!(nb_prefixes, line)
+                    nb_prefixes[line] = prefix_type
                 end
             end
             return nb_prefixes
@@ -30,21 +40,119 @@ struct SentenceSplitter
     
         if !ismissing(lang)
             if !(lang in SUPPORTED_LANG)
-                throw(ArgumentError("Unsupported language. Use a supported language " \
-                                    "($SUPPORTED_LANG). You can also provide your own " \
-                                    "non_breaking_prefixes file with the keyword " \
-                                    "argument `prefix_file`."))
+                throw(ArgumentError("Unsupported language. Use a supported language ($SUPPORTED_LANG). " * 
+                                    "You can also provide your own non_breaking_prefixes file with the " *
+                                    "keyword argument `prefix_file`."))
             else
-                append!(prefixes, _load_prefix_file(joinpath(@__DIR__, "non_breaking_prefixes/$lang.txt")))
+                merge!(prefixes, _load_prefix_file(joinpath(@__DIR__, "non_breaking_prefixes/$lang.txt")))
             end
         end
 
         if !ismissing(prefix_file)
-            append!(prefixes, _load_prefix_file(prefix_file))
+            merge!(prefixes, _load_prefix_file(prefix_file))
         end
 
         new(prefixes)
     end
+end
+
+
+function _basic_sentence_breaks(text::String)
+    # Non-period end of sentence markers (?!) followed by sentence starters
+    text = replace(text, r"([?!]) +(['\"([\u00bf\u00A1\p{Pi}]*[\p{Lu}\p{Lo}])" => s"\1\n\2")
+
+    # Multi-dots followed by sentence starters
+    text = replace(text, r"(\.[\.]+) +(['\"([\u00bf\u00A1\p{Pi}]*[\p{Lu}\p{Lo}])" => s"\1\n\2")
+
+    # Add breaks for sentences that end with some sort of punctuation inside a quote or parenthetical and are
+    # followed by a possible sentence starter punctuation and upper case
+    text = replace(text, r"([?!\.][\ ]*['\")\]\p{Pf}]+) +(['\"([\u00bf\u00A1\p{Pi}]*[\ ]*[\p{Lu}\p{Lo}])" => s"\1\n\2")
+
+    # Add breaks for sentences that end with some sort of punctuation and are followed by a sentence starter punctuation
+    # and upper case
+    text = replace(text, r"([?!\.]) +(['\"[\u00bf\u00A1\p{Pi}]+[\ ]*[\p{Lu}\p{Lo}])" => s"\1\n\2")
+
+    return text
+end
+
+
+function _is_prefix_honorific(prefix::SubString{String}, starting_punct::SubString{String}, non_breaking_prefixes::Dict{String,PrefixType})
+    # Check if \\1 is a known honorific and \\2 is empty.
+    if prefix != ""
+        if prefix in keys(non_breaking_prefixes)
+            if non_breaking_prefixes[prefix] == default
+                if starting_punct == ""
+                    return true
+                end
+            end
+        end
+    end
+    return false
+end
+
+
+function _is_numeric(prefix::SubString{String}, starting_punct::SubString{String}, next_word::SubString{String}, non_breaking_prefixes::Dict{String,PrefixType})
+    # The next word has a bunch of initial quotes, maybe a space, then either upper case or a number.
+    if prefix != ""
+        if prefix in keys(non_breaking_prefixes)
+            if non_breaking_prefixes[prefix] == numeric_only
+                if starting_punct == ""
+                    if match(r"^[0-9]+", next_word) != nothing
+                        return true
+                    end
+                end
+            end
+        end
+    end
+    return false
+end
+
+
+function split_sentence(text::String; prefixes::Dict{String, PrefixType}=Dict{String, PrefixType}(), prefix_file=missing, lang="en")
+    if text == ""
+        return []
+    end
+
+    pf = Prefixes(prefixes, prefix_file=prefix_file, lang=lang)
+
+    text = _basic_sentence_breaks(text)
+
+    # Special punctuation cases : check all remaining periods
+    words = split(text, r" +")
+    text = ""
+    for i in 1:length(words) - 1
+        m = match(r"([\w\.\-]*)(['\"\)\]\%\p{Pf}]*)(\.+)$", words[i])
+
+        if m != nothing
+            prefix = m.captures[1]
+            starting_punct = m.captures[2]
+
+            if _is_prefix_honorific(prefix, starting_punct, pf.non_breaking_prefixes)
+                # Not breaking
+            elseif match(r"(\.)[\p{Lu}\p{Lo}\-]+(\.+)$", words[i]) != nothing
+                # Not breaking - upper case acronym
+            elseif match(r"^([ ]*['\"([\u00bf\u00A1\p{Pi}]*[ ]*[\p{Lu}\p{Lo}0-9])", words[i + 1]) != nothing
+                if !_is_numeric(prefix, starting_punct, words[i + 1], pf.non_breaking_prefixes)
+                    words[i] = words[i] * "\n"
+                    # We always add a return for these unless we have a numeric non-breaker and a number start
+                end
+            end
+        end
+        text = text * words[i] * " "
+    end
+
+    # We stopped one token from the end to allow for easy look-ahead. Append it now.
+    text = text * last(words)
+
+    # Clean up spaces at head and tail of each line as well as any double-spacing
+    text = replace(text, r" +" => s" ")
+    text = replace(text, r"\n " => s"\n")
+    text = replace(text, r" \n" => s"\n")
+    text = strip(text)
+
+    sentences = split(text, "\n")
+
+    return sentences
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,41 +3,87 @@ using Test
 
 @testset "Sentencize.jl" begin
 
-# COnstructor test
+# Prefixes Constructor tests
 
-@test "Apr" in SentenceSplitter().non_breaking_prefixes     # Default : load the english prefixes
+@test "Apr" in keys(Prefixes().non_breaking_prefixes)     # Default : load the english prefixes
 
-@test "C.Q.F.D" in SentenceSplitter(lang="fr").non_breaking_prefixes    # Load specific language
+@test "C.Q.F.D" in keys(Prefixes(lang="fr").non_breaking_prefixes)    # Load specific language
 
-ss = SentenceSplitter(["test-prefix"])
-@test "test-prefix" in ss.non_breaking_prefixes &&
-      "Apr" in ss.non_breaking_prefixes        # English is also loaded
+ss = Prefixes(Dict("test-prefix" => Sentencize.default))
+@test "test-prefix" in keys(ss.non_breaking_prefixes) &&
+      "Apr" in keys(ss.non_breaking_prefixes)        # English is also loaded
 
-ss = SentenceSplitter(["test-prefix"], lang=missing)
-@test "test-prefix" in ss.non_breaking_prefixes &&
-      !("Apr" in ss.non_breaking_prefixes)
+ss = Prefixes(Dict("test-prefix" => Sentencize.default), lang=missing)
+@test "test-prefix" in keys(ss.non_breaking_prefixes) &&
+      !("Apr" in keys(ss.non_breaking_prefixes))
 
-ss = SentenceSplitter(prefix_file="test.txt")
-@test "another-test-prefix" in ss.non_breaking_prefixes &&
-      "Apr" in ss.non_breaking_prefixes
+ss = Prefixes(prefix_file="test.txt")
+@test "another-test-prefix" in keys(ss.non_breaking_prefixes) &&
+      "Apr" in keys(ss.non_breaking_prefixes)
 
-ss = SentenceSplitter(prefix_file="test.txt", lang=missing)
-@test "another-test-prefix" in ss.non_breaking_prefixes &&
-      !("Apr" in ss.non_breaking_prefixes)
+ss = Prefixes(prefix_file="test.txt", lang=missing)
+@test "another-test-prefix" in keys(ss.non_breaking_prefixes) &&
+      !("Apr" in keys(ss.non_breaking_prefixes))
 
-ss = SentenceSplitter(["test-prefix"], prefix_file="test.txt")
-@test "another-test-prefix" in ss.non_breaking_prefixes &&
-      "Apr" in ss.non_breaking_prefixes &&
-      "test-prefix" in ss.non_breaking_prefixes
+ss = Prefixes(Dict("test-prefix" => Sentencize.default), prefix_file="test.txt")
+@test "another-test-prefix" in keys(ss.non_breaking_prefixes) &&
+      "Apr" in keys(ss.non_breaking_prefixes) &&
+      "test-prefix" in keys(ss.non_breaking_prefixes)
 
-ss = SentenceSplitter(["test-prefix"], prefix_file="test.txt", lang=missing)
-@test "another-test-prefix" in ss.non_breaking_prefixes &&
-      !("Apr" in ss.non_breaking_prefixes) &&
-      "test-prefix" in ss.non_breaking_prefixes
+ss = Prefixes(Dict("test-prefix" => Sentencize.default), prefix_file="test.txt", lang=missing)
+@test "another-test-prefix" in keys(ss.non_breaking_prefixes) &&
+      !("Apr" in keys(ss.non_breaking_prefixes)) &&
+      "test-prefix" in keys(ss.non_breaking_prefixes)
 
-ss = SentenceSplitter(["test-prefix"], prefix_file="test.txt", lang="fr")
-@test "another-test-prefix" in ss.non_breaking_prefixes &&
-      "C.Q.F.D" in ss.non_breaking_prefixes &&
-      "test-prefix" in ss.non_breaking_prefixes
+ss = Prefixes(Dict("test-prefix" => Sentencize.default), prefix_file="test.txt", lang="fr")
+@test "another-test-prefix" in keys(ss.non_breaking_prefixes) &&
+      "C.Q.F.D" in keys(ss.non_breaking_prefixes) &&
+      "test-prefix" in keys(ss.non_breaking_prefixes)
+
+@test_throws ArgumentError Prefixes(lang="some-weird-language")
+
+
+# split sentences tests
+
+@test split_sentence("") == []
+
+
+# English
+
+@test split_sentence("This is a paragraph. It contains several sentences. \"But why,\" you ask?") == ["This is a paragraph.", "It contains several sentences.", "\"But why,\" you ask?"]
+
+@test split_sentence("Hey! Now.") == ["Hey!", "Now."]
+
+@test split_sentence("Hey... Now.") == ["Hey...", "Now."]
+
+@test split_sentence("Hey. Now.") == ["Hey.", "Now."]
+
+@test split_sentence("Hey.  Now.") == ["Hey.", "Now."]
+
+@test split_sentence("Hello. No. 1. No. 2. Prefix. 1. Prefix. 2. Good bye.") == ["Hello.", "No. 1.", "No. 2.", "Prefix.", "1.", "Prefix.", "2.", "Good bye."]      # Numeric only
+
+@test split_sentence("Hello. .NATO. Good bye.") == ["Hello. .NATO. Good bye."]      # Acronym
+
+@test split_sentence("Foo bar. (Baz foo.) Bar baz.") == ["Foo bar.", "(Baz foo.)", "Bar baz."]      # Sentence within bracket
+
+
+# Deutsch
+@test split_sentence("Nie hätte das passieren sollen. Dr. Soltan sagte: \"Der Fluxcompensator war doch kalibriert!\".", lang="de") == ["Nie hätte das passieren sollen.", "Dr. Soltan sagte: \"Der Fluxcompensator war doch kalibriert!\"."]
+
+
+# French
+@test split_sentence("Brookfield Office Properties Inc. (« BOPI »), dont les actifs liés aux immeubles directement...", lang="fr") == ["Brookfield Office Properties Inc. (« BOPI »), dont les actifs liés aux immeubles directement..."]
+
+# Greek
+@test split_sentence("Όλα τα συστήματα ανώτατης εκπαίδευσης σχεδιάζονται σε εθνικό επίπεδο. Η ΕΕ αναλαμβάνει κυρίως να συμβάλει στη βελτίωση της συγκρισιμότητας μεταξύ των διάφορων συστημάτων και να βοηθά φοιτητές και καθηγητές να μετακινούνται με ευκολία μεταξύ των συστημάτων των κρατών μελών.", lang="el") == ["Όλα τα συστήματα ανώτατης εκπαίδευσης σχεδιάζονται σε εθνικό επίπεδο.", "Η ΕΕ αναλαμβάνει κυρίως να συμβάλει στη βελτίωση της συγκρισιμότητας μεταξύ των διάφορων συστημάτων και να βοηθά φοιτητές και καθηγητές να μετακινούνται με ευκολία μεταξύ των συστημάτων των κρατών μελών."]
+
+# Portuguese
+@test split_sentence("Isto é um parágrafo. Contém várias frases. «Mas porquê,» perguntas tu?", lang="pt") == ["Isto é um parágrafo.", "Contém várias frases.", "«Mas porquê,» perguntas tu?"]
+
+# Espagnol
+@test split_sentence("La UE ofrece una gran variedad de empleos en un entorno multinacional y multilingüe. La Oficina Europea de Selección de Personal (EPSO) se ocupa de la contratación, sobre todo mediante oposiciones generales.", lang="es") == ["La UE ofrece una gran variedad de empleos en un entorno multinacional y multilingüe.", "La Oficina Europea de Selección de Personal (EPSO) se ocupa de la contratación, sobre todo mediante oposiciones generales."]
+
+# Custom prefixes
+@test split_sentence("Hello. Prefix1. Prefix2. Hello again. Good bye.", prefixes=Dict("Prefix1" => Sentencize.default, "#Hello" => Sentencize.default, "Prefix2" => Sentencize.default)) == ["Hello.", "Prefix1. Prefix2. Hello again.", "Good bye."]
 
 end
