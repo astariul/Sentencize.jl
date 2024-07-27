1d545647ad29bd35482c3b2717c9113890f6f70e
Documenter.jl <documenter@juliadocs.github.io>
2024-07-27 13:19:51 +0000

    build based on 9a0b39a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sentencize","category":"page"},{"location":"#Sentencize","page":"Home","title":"Sentencize","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Sentencize.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sentencize]","category":"page"}]
+}
